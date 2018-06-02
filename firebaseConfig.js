b1ac3c67870080b84bcd9d6c5d9e85c897c1f05c
facain <acainf@gmail.com>
2018-06-02 23:47:50 +0800

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyA6Qh9G4Ah3Bv1tH0va17w0bT88qb2A1oc",
+    authDomain: "whiskerssystem.firebaseapp.com",
+    databaseURL: "https://whiskerssystem.firebaseio.com",
+    projectId: "whiskerssystem",
+    storageBucket: "whiskerssystem.appspot.com",
+    messagingSenderId: "523690017399"
+  };
+  firebase.initializeApp(config)
